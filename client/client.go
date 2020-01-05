fd7cedf76c10fedea22e2e733591c9c894e4b274
fliropp <grottum@gmail.com>
2020-01-06 00:32:12 +0100

    ping over gRPC (oh yeah!)

---
+++ client/client.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	ping "github.com/fliropp/grpc_protobuf_test/ping"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/testdata"
+)
+
+var (
+	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
+	serverAddr         = flag.String("server_addr", "localhost:10000", "The server address in the format of host:port")
+	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name use to verify the hostname returned by TLS handshake")
+)
+
+func printPing(client ping.PingClient, p *ping.PingReq) {
+	log.Printf("Sedning msg ovre gRPC (%s)", p)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	png, err := client.GetPing(ctx, p)
+	if err != nil {
+		log.Fatalf("%v.GetPingReq(_) = _, %v: ", client, err)
+	}
+	log.Println(png)
+}
+
+func main() {
+	fmt.Println("gRPC ping client is running")
+	flag.Parse()
+	var opts []grpc.DialOption
+	if *tls {
+		if *caFile == "" {
+			*caFile = testdata.Path("ca.pem")
+		}
+		creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
+		if err != nil {
+			log.Fatalf("Failed to create TLS credentials %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
+
+	opts = append(opts, grpc.WithBlock())
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	client := ping.NewPingClient(conn)
+
+	// Looking for a valid feature
+	printPing(client, &ping.PingReq{Request: "ping"})
+
+}
---
+++ server/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	ping "github.com/fliropp/grpc_protobuf_test/ping"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/testdata"
+)
+
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("key_file", "", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	port       = flag.Int("port", 10000, "The server port")
+)
+
+type pingServer struct {
+	msg string
+}
+
+func (ps *pingServer) GetPing(ctx context.Context, p *ping.PingReq) (*ping.PingResp, error) {
+	return &ping.PingResp{Response: "pong"}, nil
+}
+
+func newServer() *pingServer {
+	s := &pingServer{msg: "ping"}
+	return s
+}
+
+func main() {
+	fmt.Println("gRPC ping server is running")
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = testdata.Path("server1.pem")
+		}
+		if *keyFile == "" {
+			*keyFile = testdata.Path("server1.key")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
+	ping.RegisterPingServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
+}
